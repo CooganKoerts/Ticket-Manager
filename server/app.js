910da6be8ffbc02d163055331675055987fac562
CooganKoerts <coogsk@gmail.com>
2020-08-21 20:06:40 -0400

    Added basic ticket CRUD routes

--- server/app.js
+++ server/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const dotenv = require('dotenv').config();
 
 const ticketRoutes = require('./routes/ticketRoutes');
 
-const Ticket = require('./models/ticket');
-
 const app = express();
-const dbURI = process.env.DB_URI;
 
+const dbURI = process.env.DB_URI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(5000))
     .catch((err) => console.log(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    console.log(req.path);
+    next();
+})
 
 app.use('/', ticketRoutes);
--- server/routes/ticketRoutes.js
+++ server/routes/ticketRoutes.js
@@ -4,20 +4,56 @@ const Ticket = require('../models/ticket');
 const router = express.Router();
 
 router.get('/tickets', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
     Ticket.find().sort({ createdAt: -1 })
         .then((result) => {
             res.send(result);
         })
         .catch((err) => {
+            // TODO create error response & code
             console.log(err);
         })
 });
 
 router.post('/tickets', (req, res) => {
-    
-})
+    const ticket = new Ticket(req.body);
+
+    ticket.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            // TODO create error response & code
+            console.log(err)
+        })
+});
+
+router.get('/tickets/:id', (req, res) => {
+    const id = req.params.id;
+
+    Ticket.findById(id)
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            // TODO create error response & code
+            console.log(err);
+        });
+});
+
+router.delete('/tickets/:id', (req, res) => {
+    const id = req.params.id;
+
+    console.log('DELETE');
+
+    Ticket.findByIdAndDelete(id)
+        .then(result => {
+            res.statusCode = 200;
+            res.send('Ticket successfully deleted!');
+        })
+        .catch(err => {
+            // TODO create error response & code
+            console.log(err);
+        });
+});
 
 module.exports = router;
