0dc0f0e07ae8b8b1a8adf13503894cfe25b27123
CooganKoerts <coogsk@gmail.com>
2020-08-22 23:41:57 -0400

    Added some error handling and conditionals for creating new users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,30 +6,38 @@ const router = express.Router();
 
 router.post('/register', (req, res) => {
     const { firstName, lastName, email, userName, password } = req.body;
-    // console.log(firstName);
-    // console.log(lastName);
-    // console.log(email);
-    // console.log(userName);
-    // console.log(password);
-    const newUser = new User({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        userName: userName,
-        password: password
-    });
 
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-
-            newUser.password = hash;
-            newUser.save()
-            .then((value) => {
-                console.log(value);
+    // TODO: make sure passwords match when creating a user
+    User.findOne({ email: email }).exec((err, user) => {
+        if (user) { 
+            res.statusCode = 403;
+            res.send('That email has already been registered!');
+        } else {
+            const newUser = new User({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                userName: userName,
+                password: password
             });
-        });
-    });
+        
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+        
+                    newUser.password = hash;
+                    newUser.save()
+                    .then((value) => {
+                        res.statusCode = 200;
+                        res.send('Account successfully registered!')
+                    }).catch((err) => {
+                        res.statusCode = 500;
+                        res.send('Oops! Something went wrong.')
+                    });
+                });
+            });
+        }
+    })
 });
 
 module.exports = router;
