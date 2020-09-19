7a3af26dddb9c6c79155b84296ff260388d84bf1
CooganKoerts <coogsk@gmail.com>
2020-09-19 12:42:12 -0400

    UI: added some submission logic

--- user-interface/src/pages/register/Register.jsx
+++ user-interface/src/pages/register/Register.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Field } from 'redux-form'
 
-const Register = () => {
+const Register = ({ handleSubmit }) => {
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>First Name</label>
                 <div>
@@ -15,6 +15,40 @@ const Register = () => {
                     />
                 </div>
             </div>
+            <div>
+                <label>Last Name</label>
+                <div>
+                    <Field
+                        name="lastName"
+                        component="input"
+                        type="text"
+                        placeholder="Last Name"
+                    />
+                </div>
+            </div>
+            <div>
+                <label>Email</label>
+                <div>
+                    <Field
+                        name="email"
+                        component="input"
+                        type="text"
+                        placeholder="Email"
+                    />
+                </div>
+            </div>
+            <div>
+                <label>Password</label>
+                <div>
+                    <Field
+                        name="Password"
+                        component="input"
+                        type="text"
+                        placeholder="Password"
+                    />
+                </div>
+            </div>
+            <button type="submit" onSubmit={handleSubmit}>Submit</button>
         </form>
     );
 }
--- user-interface/src/pages/register/RegisterContainer.js
+++ user-interface/src/pages/register/RegisterContainer.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Register from './Register';
+import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 
+const handleSubmit = (values, dispatch, props) => {
+    console.log(values);
+}
+
+const mapStateToProps = (state) => {
+    return {
+        state: state.data
+    }
+}
 
 export default reduxForm({
-    form: 'registration'
-})(Register);
+    form: 'registration',
+    onSubmit: handleSubmit,
+})(connect(mapStateToProps)(Register));
