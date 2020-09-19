7f44b604645d70bc2f004d9248ec974bc1745656
CooganKoerts <coogsk@gmail.com>
2020-09-19 10:22:08 -0400

    restructured files

---
+++ user-interface/src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+   componentWillMount() {
+      // TODO: check if user is already logged in on the page on load/refresh.
+   }
+   render() {
+      return <div> LandingPage </div>
+   }
+}
+
+export default LandingPage;
