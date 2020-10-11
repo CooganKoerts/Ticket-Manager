4ffe7673ee28237dc327eaac93ef9754f05ac358
CooganKoerts <coogsk@gmail.com>
2020-10-10 20:16:22 -0400

    UI: navbar styles

--- user-interface/src/pages/landingPage/LandingPage.jsx
+++ user-interface/src/pages/landingPage/LandingPage.jsx
@@ -1,3 +1,6 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Toolar from '@material-ui/core/Toolbar';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -7,8 +10,13 @@ import './LandingPage.css';
 
 const LandingPage = ({ isUserLoggedIn }) => (
   <div className="landing-page">
+    <AppBar className="navbar" position="static">
+      <Toolbar>
+        <span className="navbar-brand">Ticket Manager</span>
+      </Toolbar>
+    </AppBar>
     { !isUserLoggedIn && (
-    <AccountAuth />
+      <AccountAuth />
     )}
   </div>
 );
