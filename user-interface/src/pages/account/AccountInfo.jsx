430fbdeab3be11a6a69c8fbab972ea762dbcd0b1
CooganKoerts <coogsk@gmail.com>
2020-10-11 10:16:37 -0400

    UI: styles for user-interface

--- user-interface/src/pages/account/AccountInfo.jsx
+++ user-interface/src/pages/account/AccountInfo.jsx
@@ -1,15 +1,22 @@
 import Card from '@material-ui/core/Card';
 import React from 'react';
 
+import { CustomNavBar } from '../../components';
+
 import './AccountInfo.css';
 
 const AccountInfo = ({ user }) => (
-  <Card className="account-card">
-    <span>{user.firstname}</span>
-    <span>{user.lastname}</span>
-    <span>{user.email}</span>
-    <span>{user.username}</span>
-  </Card>
+  <>
+    <CustomNavBar />
+    <div className="account-info">
+      <Card className="account-card" raised>
+        <span><b>Firstname:</b> {user.firstname}</span>
+        <span><b>Lastname:</b> {user.lastname}</span>
+        <span><b>Email: </b>{user.email}</span>
+        <span><b>Username: </b>{user.username}</span>
+      </Card>
+    </div>
+  </>
 );
 
 export default AccountInfo;
