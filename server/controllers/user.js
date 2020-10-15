0e77812ab228ac82b8d6a57dda304c41afc32b88
CooganKoerts <coogsk@gmail.com>
2020-10-14 20:02:37 -0400

    Server: User routes controller

---
+++ server/controllers/user.js
@@ -0,0 +1,58 @@
+const User = require('../models/user');
+const { handleErrors } = require('../errors/handleErrors');
+const { createToken } = require('../helpers/createToken');
+
+const registerPost = async (req, res) => {
+    const { firstname, lastname, email, username, password } = req.body;
+    const path = '/register';
+    console.log(path);
+
+    try {
+        const user = await User.create({ firstname, lastname, email, username, password });
+        const token = createToken(user._id);
+        console.log(token);
+        console.log(`${path}: token created successfully`);
+        res.status(201).json({
+            'access-validation-id': token,
+            user: { 
+                id: user._id, 
+                firstname: user.firstname,
+                lastname: user.lastname,
+                username: user.username,
+                email: user.email, 
+            }
+        });
+    } catch (err) {
+        console.log(`${path} Error: ${err}`)
+        const errors = handleErrors(err);
+        res.status(400).json({ errors })
+    }
+}
+
+const loginPost = async (req, res) => {
+    const { email, password } = req.body;
+    const path = '/login';
+    console.log(path);
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        console.log(`${path}: token created successfully`);
+        res.status(200).json({ 
+            'access-validation-id': token,
+            user: { 
+                id: user._id, 
+                firstname: user.firstname,
+                lastname: user.lastname,
+                username: user.username,
+                email: user.email, 
+            }
+        });
+    } catch (err) {
+        console.log(`${path} Error: ${err}`)
+        const errors = handleErrors(err);
+        res.status(400).json({ errors: errors });
+    }
+}
+
+module.exports = { registerPost, loginPost }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,63 +1,9 @@
 const express = require('express');
-const User = require('../models/user');
-const { handleErrors } = require('../errors/handleErrors');
-const { createToken } = require('../helpers/createToken');
-
-const { TOKEN_MAX_AGE } = process.env;
+const { registerPost, loginPost } = require('../controllers/user');
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    const { firstname, lastname, email, username, password } = req.body;
-    const path = '/register';
-    console.log(path);
-
-    try {
-        const user = await User.create({ firstname, lastname, email, username, password });
-        const token = createToken(user._id);
-        console.log(token);
-        console.log(`${path}: token created successfully`);
-        res.status(201).json({
-            'access-validation-id': token,
-            user: { 
-                id: user._id, 
-                firstname: user.firstname,
-                lastname: user.lastname,
-                username: user.username,
-                email: user.email, 
-            }
-        });
-    } catch (err) {
-        console.log(`${path} Error: ${err}`)
-        const errors = handleErrors(err);
-        res.status(400).json({ errors })
-    }
-});
-
-router.post('/login', async (req, res, next) => {
-    const { email, password } = req.body;
-    const path = '/login';
-    console.log(path);
-
-    try {
-        const user = await User.login(email, password);
-        const token = createToken(user._id);
-        console.log(`${path}: token created successfully`);
-        res.status(200).json({ 
-            'access-validation-id': token,
-            user: { 
-                id: user._id, 
-                firstname: user.firstname,
-                lastname: user.lastname,
-                username: user.username,
-                email: user.email, 
-            }
-        });
-    } catch (err) {
-        console.log(`${path} Error: ${err}`)
-        const errors = handleErrors(err);
-        res.status(400).json({ errors: errors });
-    }
-});
+router.post('/register', registerPost);
+router.post('/login', loginPost);
 
 module.exports = router;
