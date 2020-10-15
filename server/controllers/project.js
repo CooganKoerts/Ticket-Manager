184e953eca7b298eb8d396f0c5411dfb7d5e7471
CooganKoerts <coogsk@gmail.com>
2020-10-14 20:16:14 -0400

    Server: project route controller

---
+++ server/controllers/project.js
@@ -0,0 +1,66 @@
+const Project = require('../models/project');
+
+const path = '/project';
+
+const projectGet = (req, res) => {
+    console.log(`${path} GET`);
+
+    Project.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            // TODO create error response & code
+            console.log(`${path} GET Error: ${err}`);
+        })
+};
+
+const projectPost = (req, res) => {
+    const project = new Project(req.body);
+    console.log(`${path} POST`);
+
+    project.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            // TODO create error response & code
+            console.log(`${path} POST Error: ${err}`);
+        })
+};
+
+const projectIdGet = (req, res) => {
+    const id = req.params.id;
+    console.log(`${path}/:id GET`)
+
+    project.findById(id)
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            // TODO create error response & code
+            console.log(`${path}/:id GET Error: ${err}`);
+        });
+};
+
+const projectIdDelete = (req, res) => {
+    const id = req.params.id;
+    console.log(`${path}/:id DELETE`)
+
+    project.findByIdAndDelete(id)
+        .then(result => {
+            res.statusCode = 200;
+            res.send('Project successfully deleted!');
+        })
+        .catch(err => {
+            // TODO create error response & code
+            console.log(`${path}/:id DELETE Error: ${err}`);
+        });
+};
+
+module.exports = {  
+    projectGet,
+    projectPost,
+    projectIdGet,
+    projectIdDelete
+};
--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,64 +1,16 @@
 const express = require('express');
-const Project = require('../models/project');
+const { 
+    projectGet,
+    projectPost,
+    projectIdGet,
+    projectIdDelete
+} = require('../controllers/project');
 
 const router = express.Router();
 
-const path = '/project'
-
-router.get('/project', (req, res) => {
-    console.log(`${path} GET`);
-
-    Project.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            // TODO create error response & code
-            console.log(`${path} GET Error: ${err}`);
-        })
-});
-
-router.post('/project', (req, res) => {
-    const project = new Project(req.body);
-    console.log(`${path} POST`);
-
-    project.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            // TODO create error response & code
-            console.log(`${path} POST Error: ${err}`);
-        })
-});
-
-router.get('/project/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(`${path}/:id GET`)
-
-    Project.findById(id)
-        .then(result => {
-            res.send(result);
-        })
-        .catch(err => {
-            // TODO create error response & code
-            console.log(`${path}/:id GET Error: ${err}`);
-        });
-});
-
-router.delete('/project/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(`${path}/:id DELETE`)
-
-    Project.findByIdAndDelete(id)
-        .then(result => {
-            res.statusCode = 200;
-            res.send('Project successfully deleted!');
-        })
-        .catch(err => {
-            // TODO create error response & code
-            console.log(`${path}/:id DELETE Error: ${err}`);
-        });
-});
+router.get('/project', projectGet);
+router.post('/project', projectPost);
+router.get('/project/:id', projectIdGet);
+router.delete('/project/:id', projectIdDelete);
 
 module.exports = router;
